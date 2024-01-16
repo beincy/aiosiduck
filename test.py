ca815c024b4b844051b1a24e5edc32ae54a37b15
BIANHUIWORK\bianhui <bianhui0524@sina.com>
2024-01-16 15:30:08 +0800

    feat: init

---
+++ test.py
@@ -0,0 +1,15 @@
+import asyncio
+from aiosiduck import SiduckClient
+
+
+async def main():
+    client = SiduckClient("127.0.0.1", "8088", "cert.pem")
+    res = await client.request(b"i am a cat")
+    print(res)
+    res = await client.request(b"i am a dog")
+    print(res)
+    await client.close()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
